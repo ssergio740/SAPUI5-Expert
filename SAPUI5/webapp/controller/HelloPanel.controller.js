aba824e6cdccb01b4f15871c4155b42ecef64c07
ssergio740 <sesierrag@unal.edu.co>
2025-12-03 22:07:52 +0000

    Dialogos e iconos

--- SAPUI5/webapp/controller/HelloPanel.controller.js
+++ SAPUI5/webapp/controller/HelloPanel.controller.js
@@ -1,24 +1,48 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast"
+    "sap/m/MessageToast",
+    "sap/ui/core/Fragment"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      * @param {typeof sap.m.MessageToast} MessageToast
+     * @param {typeof sap.ui.core.Fragment}Fragment
      */
-    function (Controller, MessageToast) {
+    function (Controller, MessageToast, Fragment) {
         "use strict";
 
         return Controller.extend("project1.SAPUI5.controller.HelloPanel", {
-            
-            onInit: function (){
-                },
+
+            onInit: function () {
+            },
             onShowHello: function () {
                 //read rext from 18n
                 var oBundle = this.getView().getModel("i18n").getResourceBundle();
                 var sRecipient = this.getView().getModel().getProperty("/recipient/name");
-                var sMsg = oBundle.getText("helloMessage",[sRecipient]); 
+                var sMsg = oBundle.getText("helloMessage", [sRecipient]);
                 MessageToast.show(sMsg);
+            },
+
+            onOpenDialog: function () {
+                const oView = this.getView();
+                if (!this.byId("helloDialog")) {
+                    Fragment.load({
+                        id: oView.getId(),
+                        name: "project1.SAPUI5.view.HelloDialog",
+                        controller: this
+                    }).then(function (oDialog) {
+                        oView.addDependent(oDialog);
+                        oDialog.open();
+                    });
+                } else {
+                    this.byId("helloDialog").open()
+                }
+
+            },
+
+            onCloseDialog: function(){
+                this.byId("helloDialog").close();
+
             }
         });
     });
